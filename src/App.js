b5dc14ac1a479dfb76b727856f1ea6b315604f4c
Rajat Pathak <rajatpathak786@gmail.com>
2019-05-15 10:43:40 +0530

    files modified

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import AddEmployee from './component/addEmployee'
 import EditEmployee from './component/editEmployee'
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 function Home() {
   return <h2>You are on Home Page</h2>
 }
 
+
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <nav>
-            <label><Link to = "/">Home</Link></label>
-            <label><Link to = "/addEmp">AddEmployee</Link></label>
-            <label><Link to = "/editEmp">EditEmployee</Link></label>      
-          </nav>
-          <Route path = "/" exact component = {Home} />
-          <Route path = "/addEmp" exact component = {AddEmployee} />
-          <Route path = "/editEmp" exact component = {EditEmployee} />
-        </div>  
+        <Grid 
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '100vh' }}>
+          <div>
+            <nav>
+              <Grid container justify = "center" spacing = {16}>
+                <label><Link to = "/">Home</Link></label>
+                <label><Link to = "/addEmp">AddEmployee</Link></label>
+                <label><Link to = "/editEmp">EditEmployee</Link></label>
+              </Grid>      
+            </nav>
+            <Route path = "/" exact component = {Home} />
+            <Route path = "/addEmp" exact component = {AddEmployee} />
+            <Route path = "/editEmp" exact component = {EditEmployee} />
+          </div>  
+        </Grid>
       </Router>
     );
   }
--- src/component/addEmployee.js
+++ src/component/addEmployee.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 class AddEmployee extends Component {
   render() {
     return (
-      <div>
-        <form id = 'addEmp'>
-          <p>Name: <input type = "Text" id = "name"/></p>
-          <p>E-Mail: <input type = "Text" id = "email"/></p>
-        </form>
-          <input type = "Submit" />
-      </div>
+      <Grid 
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '100vh' }}>
+        <div>
+          <form id = 'addEmp'>
+            <p>Name: <input type = "Text" id = "name"/></p>
+            <p>E-Mail: <input type = "Text" id = "email"/></p>
+          </form>
+          <Button variant="contained" color="primary">
+            <input type = "Submit" />
+          </Button>
+        </div>
+      </Grid>
     )
   }
 }
