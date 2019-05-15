ac3632133b471073dd5087ff368024b79ff76a80
Rajat Pathak <rajatpathak786@gmail.com>
2019-05-15 10:43:40 +0530

    files modified

--- src/component/editEmployee.js
+++ src/component/editEmployee.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 class EditEmployee extends Component {
 render() {
   return(
-    <div>
-      Edit Page:
-      <form id = "editEmp">
-        <p>Emp Id: <input type = "Number" id = "empId" /></p>
-        <p>Name: <input type = "Text" id = "name" /></p>
-        <p>E-Mail: <input type = "Text" id = "email" /></p>
-      </form>
-        <input type = "Submit" />
-    </div>
+    <Grid 
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '100vh' }}>
+      <div>
+        Edit Page:
+        <form id = "editEmp">
+          <p>Emp Id: <input type = "Number" id = "empId" /></p>
+          <p>Name: <input type = "Text" id = "name" /></p>
+          <p>E-Mail: <input type = "Text" id = "email" /></p>
+        </form>
+          <Button variant = "contained" color = "primary">
+            <input type = "Submit" />
+          </Button>
+      </div>
+    </Grid>
   )    
 }
 }
